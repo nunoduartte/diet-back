18e058e81d95a4734ccf6df5a80327eb7f5d7d15
cadu <carlosvicc14@gmail.com>
2021-09-01 10:43:17 -0300

    first commit

---
+++ src/main/java/br/ufrj/DietBackApplication.java
@@ -0,0 +1,13 @@
+package br.ufrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietBackApplication.class, args);
+    }
+
+}
