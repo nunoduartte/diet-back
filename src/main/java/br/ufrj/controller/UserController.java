98970ccd6994319ae6a42ee31ada36229311c7db
ncorreia <ncorreia.ufrj@gmail.com>
2021-09-01 15:06:25 -0300

    Adicionando swagger

--- src/main/java/br/ufrj/controller/UserController.java
+++ src/main/java/br/ufrj/controller/UserController.java
@@ -5,14 +5,16 @@ import br.ufrj.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user")
+    @PostMapping
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
--- src/main/java/br/ufrj/model/User.java
+++ src/main/java/br/ufrj/model/User.java
@@ -3,14 +3,12 @@ package br.ufrj.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
+@Table(name="user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
