44db6decd944845800fc5b4a5382a26ddb22648e
ncorreia <ncorreia.ufrj@gmail.com>
2021-09-01 16:07:41 -0300

    Merge

--- src/main/java/br/ufrj/controller/UserController.java
+++ src/main/java/br/ufrj/controller/UserController.java
@@ -1,19 +1,34 @@
 package br.ufrj.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import br.ufrj.model.User;
 import br.ufrj.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user")
+    @PostMapping
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> userLogin(@RequestParam(required = false) String username,
+                                          @RequestParam(required = false) String email,
+                                          @RequestParam(required = false) String password) {
+
+        User userLogin = userRepository.loginUser(username, email, password);
+
+        if(userLogin == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().body(userLogin);
+    }
+
 }
