3cbbc9b50a0902f169e412b014512e729f53958a
Kevin <kevinvilardefaria@gmail.com>
2021-10-13 07:20:01 -0300

    Permite remover alimento

--- src/main/java/br/ufrj/controller/DietController.java
+++ src/main/java/br/ufrj/controller/DietController.java
@@ -1,7 +1,10 @@
 package br.ufrj.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,12 @@ public class DietController {
     public Food createFood(@RequestBody Food food){
 
         return foodRepository.save(food);
+    }
+    
+    @DeleteMapping("/food/{id}")
+    @CrossOrigin
+    public void deleteFood(@PathVariable(value = "id") Long foodId){
+
+        foodRepository.deleteById(foodId);
     }
 }
