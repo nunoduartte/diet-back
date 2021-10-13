635b245cb345a0f61344ad5c2492f4634b3a7bef
Kevin <kevinvilardefaria@gmail.com>
2021-10-13 07:41:49 -0300

    Permite editar alimento

--- src/main/java/br/ufrj/controller/DietController.java
+++ src/main/java/br/ufrj/controller/DietController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,13 @@ public class DietController {
     @PostMapping("/food")
     @CrossOrigin
     public Food createFood(@RequestBody Food food){
+
+        return foodRepository.save(food);
+    }
+    
+    @PutMapping("/food")
+    @CrossOrigin
+    public Food updateFood(@RequestBody Food food){
 
         return foodRepository.save(food);
     }
